25dac00d2173552dbff5dd49ad915f10c32b44c1
Abdullah Raja <abra73584@gmail.com>
2026-09-07 21:18:17 +0100

    added placeholder dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,14 @@
 export function DashboardPage() {
   return (
-    <section>
-      <p className="eyebrow">Games</p>
-      <h1>Dashboard</h1>
-      <p>Game creation, joining page...</p>
-    </section>
+    <body>
+      <section>
+        <p className="eyebrow">Games</p>
+        <h1>Dashboard</h1>
+        <p>Game creation, joining page...</p>
+      </section>
+      <footer>
+        <p>Copyright 2026</p>
+      </footer>
+    </body>
   );
 }
