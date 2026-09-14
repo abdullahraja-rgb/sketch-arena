7ce730dd06db31f0f7ce7ed76c638327f94f4aa4
Abdullah Raja <abra73584@gmail.com>
2026-09-15 00:56:02 +0100

    wip: adding testcontainers for integration testing using a postgre db

--- backend/src/test/java/com/sketcharena/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/sketcharena/backend/BackendApplicationTests.java
@@ -3,11 +3,34 @@ package com.sketcharena.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+// an extension on junit jupiter (doesnt support parallel test exec)
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
 @SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Container
+	static final PostgreSQLContainer POSTGRES = new PostGreSQLContainer("postgres:18-alpine").withDatabaseName("sketch_arena_test").withUsername("sketch_arena").withPassword("sketch_arena_test");
+
+	@DynamicPropertySource
+	static void databaseProperties(DynamicProperyRegistry registry) {
+		registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+		registry.add("spring.datasource.username", POSTGRES::getUsername);
+		registry.add("spring.datasource.password", POSTGRES::getPassword);
 	}
 
 }
